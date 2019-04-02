58fd3c55f9993869ad8b368b9aeaaa2167469023
Adam Oliveras <theadamoliveras@gmail.com>
2019-04-02 16:36:58 -0700

    changed navbar color to red and moved a portion of the links to the right hand side

--- dysm/src/components/common/Navbar/Navbar.js
+++ dysm/src/components/common/Navbar/Navbar.js
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom';
 export default (Layout) => {
     return (
         <div className="column">
-            <nav className="ui inverted teal huge menu">
-                <Link className="item" to="/">doyouseeme.com</Link>
-                <Link className="teal item" to="/about">About</Link>
-                <Link className="teal item" to="/art">Art</Link>
-                <Link className="teal item" to="/dreamBoard">Dream Board</Link>
+            <nav className="ui inverted red huge menu">
+                <Link className="item" to="/">doyouSEEme.com</Link>
+                <Link className="red item" to="/about">About</Link>
+                <div className="right menu">
+                    <Link className="red item" to="/art">Art</Link>
+                    <Link className="red item" to="/dreamBoard">Dream Board</Link>
+                </div>
             </nav>
         </div>
     )
