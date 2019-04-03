d82d90d1b234f134fc0d455c6773bee6e97a1749
Adam Oliveras <theadamoliveras@gmail.com>
2019-04-03 15:40:46 -0700

    inserted dysm logo into Layout, playing with layout ideas

--- dysm/src/components/Home/Home.js
+++ dysm/src/components/Home/Home.js
@@ -4,7 +4,12 @@ export class Home extends Component {
   render() {
     return (
       <div>
-        <h2>Home Component</h2>
+        <div className="ui container">
+            <div className="ui huge header segment stacked"  style={{ textAlign: "center" }}>
+                <h2>Home Component</h2>
+                <p>This is the splash page!</p>
+            </div>
+        </div>
       </div>
     )
   }
--- dysm/src/components/common/Layout/Layout.js
+++ dysm/src/components/common/Layout/Layout.js
@@ -4,7 +4,7 @@ import Navbar from '../Navbar/Navbar';
 export default ({ children }) => {
     return (
         <div className="ui container">
-            <h1>Do You SEE Me?</h1>
+            <img src={require("./dysm_icon.png")} alt="dysm_icon" style={{ display: "block", margin: "20px auto" }} />
             <Navbar />
             <div className="ui container">
                 { children }
